40789d239f1ceaed4ba894d0b3c500dea6b96128
andrew-kim99 <35050620+andrew-kim99@users.noreply.github.com>
2020-02-18 10:14:52 -0800

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/drewnofluno/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/drewnofluno/AndroidStudioProjects/day1_flutter_app"
+export "FLUTTER_TARGET=/Users/drewnofluno/AndroidStudioProjects/day1_flutter_app/lib/Screen1.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/drewnofluno/Downloads/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
